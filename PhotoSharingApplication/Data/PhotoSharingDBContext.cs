77078d96af4b0f3f92199b0da0a15e6b885bf66a
Mariela Valentini <marielavalentini@outlook.com>
2022-11-25 16:21:03 -0300

    Agregar archivos de proyecto.

---
+++ PhotoSharingApplication/Data/PhotoSharingDBContext.cs
@@ -0,0 +1,18 @@
+﻿using PhotoSharingApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PhotoSharingApplication.Data
+{
+    public class PhotoSharingDBContext : DbContext
+    {
+        public PhotoSharingDBContext() : base("keyPhotoSharingDB") { }
+
+        public DbSet<Photo> Photos { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+    }
+}
