776c7e243bc6312dca8d297e6299ed216a595149
Mariela Valentini <marielavalentini@outlook.com>
2022-11-28 19:43:03 -0300

    Views

--- PhotoSharingApplication/Controllers/PhotoController.cs
+++ PhotoSharingApplication/Controllers/PhotoController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Collections.Generic;
 using System.Globalization;
 using PhotoSharingApplication.Models;
 using PhotoSharingApplication.Data;
@@ -17,9 +16,29 @@ namespace PhotoSharingApplication.Controllers
         // GET: Photo
         public ActionResult Index()
         {
-            return View("Index", context.Photos.ToList());
+            //return View("Index", context.Photos.ToList());
+            return View("Index");
+        }
+
+        [ChildActionOnly]
+        public ActionResult _PhotoGallery (int number = 0)
+        {
+            List<Photo> photos;
+            if (number == 0)
+            {
+                photos = context.Photos.ToList();
+            }
+            else
+            {
+                photos = (from p in context.Photos
+                          orderby p.CreatedDate descending
+                          select p).Take(number).ToList();
+            }
+
+            return PartialView("_PhotoGallery",photos);
 
         }
+
         public ActionResult Display(int id)
         {
             Photo photo = context.Photos.Find(id);
